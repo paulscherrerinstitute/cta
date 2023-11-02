601bc91f0d5d95bc4d09ad728d05c35f7fb264cd
Roger Biffiger <roger.biffiger@psi.ch>
2023-11-02 12:00:03 +0100

    Fixed python entry points

--- lib/setup.py
+++ lib/setup.py
@@ -13,10 +13,10 @@ setup(
     requires=['pyepics', 'numpy'],
     entry_points={
         'console_scripts': [
-            'cta_lib_ex01_upload_downlaod_print = cta_lib.cta_lib_ex01_upload_downlaod_print:main'
-            'cta_lib_ex02_upload_repeat_n_wait = cta_lib.cta_lib_ex02_upload_repeat_n_wait:main'
-            'cta_lib_ex03_upload_run_forever_stop = cta_lib.cta_lib_ex03_upload_run_forever_stop:main'
-            'cta_lib_ex04_callbacks = cta_lib.cta_lib_ex04_callbacks:main'
+            'cta_lib_ex01_upload_download_print = cta_lib.cta_lib_ex01_upload_download_print:main',
+            'cta_lib_ex02_upload_repeat_n_wait = cta_lib.cta_lib_ex02_upload_repeat_n_wait:main',
+            'cta_lib_ex03_upload_run_forever_stop = cta_lib.cta_lib_ex03_upload_run_forever_stop:main',
+            'cta_lib_ex04_callbacks = cta_lib.cta_lib_ex04_callbacks:main',
         ]
     },
 )
