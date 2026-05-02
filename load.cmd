7fb99e076f9ee7359fe38e111772ee9cf62325a9
Romain Ludovic Vallotton <vallotton_r@sf-lc8a.psi.ch>
2026-05-02 10:12:30 +0200

    [DEV] working version with autostart on 1 sequence

--- load.cmd
+++ load.cmd
@@ -3,4 +3,4 @@ require seq
 dbLoadTemplate("cta.template", "SYS=$(SYS), DEVICE=$(DEVICE), DN=$(DN), EOS=$(EOS), PIR=$(PIR)")
 
 
-#seq &ctaSeq, "DN=$(DN):,EOS=$(EOS):"
+seq &ctaSeq, "DN=$(DN):,EOS=$(EOS):"
