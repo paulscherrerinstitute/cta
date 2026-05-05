df931fbe7145dc4a9c1bf5d8fdfb835b71b27959
Romain <romain.vallotton@psi.ch>
2026-05-05 18:45:35 +0200

    [FIX] processing order for first sequence - reorder aSub I/O

--- src/cta.c
+++ src/cta.c
@@ -12,26 +12,25 @@ typedef enum {
 	STOPPED
 } state_t;
 
-/* State variable */
-static state_t state = IDLE;
-
-int i=0;
+/* static variables */
+static                    state_t state = IDLE;
+static unsigned long long last_pid      = 0;
  
 long cta_state_machine(aSubRecord* prec) {
 
 	// Inputs
 	unsigned short start      = *(unsigned short*)     prec->a;
 	unsigned short stop       = *(unsigned short*)     prec->b;
-	unsigned short length     = *(unsigned short*)     prec->c;
-	unsigned short cycles     = *(unsigned short*)     prec->d;
-	unsigned short cfgMod     = *(unsigned short*)     prec->e;
-	unsigned short cfgModDiv  = *(unsigned short*)     prec->f;
-	unsigned short cfgModOff  = *(unsigned short*)     prec->g;
-	unsigned long long cfgPid = *(unsigned long long*) prec->h;
-	unsigned short index      = *(unsigned short*)     prec->i;
-	unsigned short running    = *(unsigned short*)     prec->j;
-	unsigned short started_at = *(unsigned short*)     prec->k;
-	unsigned short missed_pid = *(unsigned short*)     prec->l;
+	unsigned short index      = *(unsigned short*)     prec->c;
+	unsigned short running    = *(unsigned short*)     prec->d;
+	unsigned long long pid    = *(unsigned long long*) prec->e;
+	unsigned short started_at = *(unsigned short*)     prec->f;
+	unsigned short missed_pid = *(unsigned short*)     prec->g;
+	unsigned short length     = *(unsigned short*)     prec->h;
+	unsigned short cycles     = *(unsigned short*)     prec->i;
+	unsigned short cfgMod     = *(unsigned short*)     prec->j;
+	unsigned short cfgModDiv  = *(unsigned short*)     prec->k;
+	unsigned short cfgModOff  = *(unsigned short*)     prec->l;
       
         // Outputs
 	unsigned short *out_start;
@@ -57,24 +56,24 @@ long cta_state_machine(aSubRecord* prec) {
 	else if(running) state = RUNNING;
 	else             state = IDLE; 
 
+	// State machine 
 	switch(state) {
-
 		case STOPPED: 
-			*out_enable_evt=0;  // disable events
-			*out_running=0;     // update status 'running'
-			*out_index=0;       // reset index
-			*out_stop=0;        // reset stop button
+			*out_enable_evt=0;  		// disable events
+			*out_running=0;     		// update status 'running'
+			*out_index=0;      		// reset index
+			*out_stop=0;        		// reset stop button
 			break;
 		case STARTED:
-			*out_enable_evt=1;  // enable events
-			*out_running=1;     // update status 'running'
-			*out_load_seq=1;    // load sequence flag
-			*out_start=0;       // reset start button
+			*out_enable_evt=1;  		// enable events
+			*out_running=1;     		// update status 'running'
+			*out_load_seq=1;    		// load sequence flag
+			*out_start=0;       		// reset start button
+			*out_started_at=pid;            // update starting pid
 			break;
 		case RUNNING:
-			*out_load_seq=0;    // disable load sequence flag
-			*out_running=1;	    // update status 'running'	
-			*out_index=++index; // increment index
+			*out_load_seq=0;    		// disable load sequence flag
+			*out_index=++index; 		// increment index
 			if(index == length) {
 				*out_stop=1;
 				*out_enable_evt=0;
@@ -82,18 +81,10 @@ long cta_state_machine(aSubRecord* prec) {
 			else {
 				*out_enable_evt=1;
 			}
+			if(last_pid == pid)
+				++*out_missed_pid;
 			break;
-
 	}
-/*
-	if(i%100==0) {
-		errlogPrintf(
-				"start:%d, stop:%d, len:%d, cycles:%d, cfgMod:%d, cfgModDiv:%d, cfgModOff:%d, out_load_seq=%d, cfgPid:%llu STATE:%d\n",
-				start,stop,length,cycles,cfgMod,cfgModDiv,cfgModOff,*out_load_seq,cfgPid,state
-				);
-	}
-	i++;
-*/	
-
+	last_pid=pid;
 	return 0;
 }
