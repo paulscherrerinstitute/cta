fb8b562c9201710e4027a72e3aef76adc87dbdb8
Romain Ludovic Vallotton <vallotton_r@sf-lc8a.psi.ch>
2026-05-15 14:24:29 +0200

    [FIX] issue with missed event after startup, now load sequence when modified and in IDLE state. Clean up variable types too

--- src/cta.c
+++ src/cta.c
@@ -12,55 +12,51 @@ typedef enum {
 	STOPPED
 } state_t;
 
-/* static variables */
-static unsigned long long last_pid = 0;
- 
 long cta_state_machine(aSubRecord* prec) {
 
-	unsigned short next_index=0;
 	state_t state;
 
 	// Inputs
-	unsigned short start          = *(unsigned short*)     prec->a;
-	unsigned short stop           = *(unsigned short*)     prec->b;
-	unsigned short index          = *(unsigned short*)     prec->c;
-	unsigned short running        = *(unsigned short*)     prec->d;
-	unsigned long long pid        = *(unsigned long long*) prec->e;
-	unsigned long long started_at = *(unsigned long long*) prec->f;
-	unsigned short missed_pid     = *(unsigned short*)     prec->g;
-	unsigned short length         = *(unsigned short*)     prec->h;
-	unsigned short cycles         = *(unsigned short*)     prec->i;
-	unsigned short cfgMod         = *(unsigned short*)     prec->j;
-	unsigned short cfgModDiv      = *(unsigned short*)     prec->k;
-	unsigned short cfgModOff      = *(unsigned short*)     prec->l;
+	epicsUInt16 start          = *(epicsUInt16*) prec->a;
+	epicsUInt16 stop           = *(epicsUInt16*) prec->b;
+	epicsUInt32 index          = *(epicsUInt32*) prec->c;
+	epicsUInt32 running    	   = *(epicsUInt32*) prec->d;
+	epicsUInt64 pid            = *(epicsUInt64*) prec->e;
+	epicsUInt64 started_at     = *(epicsUInt64*) prec->f;
+	epicsUInt32 length         = *(epicsUInt32*) prec->g;
+	epicsUInt32 cycles         = *(epicsUInt32*) prec->h;
+	epicsUInt32 cfgMod         = *(epicsUInt32*) prec->i;
+	epicsUInt32 cfgModDiv      = *(epicsUInt32*) prec->j;
+	epicsUInt32 cfgModOff      = *(epicsUInt32*) prec->k;
+	epicsUInt16 loadSeqPending = *(epicsUInt16*) prec->l;
       
         // Outputs
-	unsigned short     *out_start;
-	unsigned short     *out_stop;
-	unsigned short     *out_index;
-	unsigned short     *out_running;
-	unsigned long long *out_started_at;
-	unsigned short     *out_missed_pid;
-	unsigned short     *out_load_seq;
-	unsigned short     *out_enable_evt;
-	out_start      = (unsigned short*)     prec->vala;
-	out_stop       = (unsigned short*)     prec->valb;
-	out_index      = (unsigned short*)     prec->valc;
-	out_running    = (unsigned short*)     prec->vald;
-	out_started_at = (unsigned long long*) prec->vale;
-	out_missed_pid = (unsigned short*)     prec->valf;
-	out_load_seq   = (unsigned short*)     prec->valg;
-	out_enable_evt = (unsigned short*)     prec->valh;
+	epicsUInt16 *out_start;
+	epicsUInt16 *out_stop;
+	epicsUInt32 *out_index;
+	epicsUInt32 *out_running;
+	epicsUInt64 *out_started_at;
+	epicsUInt16 *out_load_seq;
+	epicsUInt16 *out_load_seq_pending;
+	epicsUInt16 *out_enable_evt;
+	out_start            = (epicsUInt16*) prec->vala;
+	out_stop             = (epicsUInt16*) prec->valb;
+	out_index            = (epicsUInt32*) prec->valc;
+	out_running          = (epicsUInt32*) prec->vald;
+	out_started_at       = (epicsUInt64*) prec->vale;
+	out_load_seq         = (epicsUInt16*) prec->valf;
+	out_load_seq_pending = (epicsUInt16*) prec->valg;
+	out_enable_evt       = (epicsUInt16*) prec->valh;
 
 	// Initialize ALL outputs
-	*out_start       = start;
-	*out_stop        = stop;
-	*out_index       = index;
-	*out_running     = running;
-	*out_started_at  = started_at;
-	*out_missed_pid  = missed_pid;
-	*out_load_seq    = 0;
-	*out_enable_evt  = 0;
+	*out_start            = start;
+	*out_stop             = stop;
+	*out_index            = index;
+	*out_running          = running;
+	*out_started_at       = started_at;
+	*out_load_seq         = 0;
+	*out_load_seq_pending = loadSeqPending;
+	*out_enable_evt       = 0;
 
 	if(length == 0)
 		return 0;
@@ -74,45 +70,39 @@ long cta_state_machine(aSubRecord* prec) {
 	// State machine 
 	switch(state) {
 		case STOPPED: 
-			*out_enable_evt=0;  		// disable events
-			*out_running=0;     		// update status 'running'
-			*out_index=0;      		// reset index
-			*out_stop=0;        		// reset stop button
+			*out_enable_evt=0;  		 // disable events
+			*out_running=0;     		 // update status 'running'
+			*out_index=0;      		 // reset index
+			*out_stop=0;        		 // reset stop button
 			break;
 		case STARTED:
-			*out_enable_evt=1;  		// enable events
-			*out_running=1;     		// update status 'running'
-			*out_load_seq=1;    		// load sequence flag
-			*out_start=0;       		// reset start button
-			*out_started_at=pid;            // update starting pid
+			*out_enable_evt=1;  		 // enable events
+			*out_running=1;     		 // update status 'running'
+			*out_start=0;       		 // reset start button
+			*out_started_at=pid;             // update starting pid
 			break;
 		case RUNNING:
-			next_index = index + 1;
-
-    			// stop BEFORE invalid index
-    			if(next_index >= length) {
+			++index;
+        		*out_index = index;
+    			if(index >= length) {
 				state=STOPPED;
         			*out_stop       = 1;
         			*out_running    = 0;
         			*out_enable_evt = 0;
-
-        			// keep last VALID index
-        			if(length > 0)
-            				*out_index = length - 1;
-        			else
-            				*out_index = 0;
     			}
     			else {
-			
-        			*out_index      = next_index;
         			*out_enable_evt = 1;
     			}
 
     			break;
 		case IDLE:
+			if(loadSeqPending){
+			        errlogPrintf("loadSeqPending = %d\n",loadSeqPending);
+				*out_load_seq=1; 	 // load sequence flag
+				*out_load_seq_pending=0; // reset load seq pending flag
+			}
 		default:
 			break;
 	}
-	last_pid=pid;
 	return 0;
 }
