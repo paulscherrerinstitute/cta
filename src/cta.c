797926b0b5f58e8c6d048a3d4b38f65923a21f12
Romain Ludovic Vallotton <vallotton_r@sf-lc8a.psi.ch>
2026-05-16 01:01:40 +0200

    [ADD] Sequence configuration - offset and divisor

--- src/cta.c
+++ src/cta.c
@@ -1,10 +1,8 @@
 #include <aSubRecord.h>
 #include <dbDefs.h>
 #include <stdlib.h>
-#include <errlog.h>
 #include <dbAccess.h>
 
-/* Define states */
 typedef enum {
 	IDLE=0,
 	ARMED,
@@ -13,9 +11,10 @@ typedef enum {
 	STOPPED
 } state_t;
 
-/* Static variables */
 static unsigned short     cycles_cnt = 0; // Counter for CTA cycles
 static unsigned long long last_pid   = 0; // Last Pulse ID
+static unsigned long long saved_pid  = 0; // Saved Pulse ID for sequence offset calculation
+static unsigned long long next_pid   = 0; // Next Pulse ID 
 static state_t            state      = 0; // State machine current state
 static state_t            last_state = 0; // State machine last state
 
@@ -70,10 +69,10 @@ long cta_state_machine(aSubRecord* prec) {
 		return 0;
 
 	// State update
-	if(stop)                         state = STOPPED;
-	else if(start && state == ARMED) state = STARTED;
-	else if(running)                 state = RUNNING;
-	else if(state != ARMED)          state = IDLE;
+	if(stop)                                               state = STOPPED;
+	else if(start && (state == ARMED || state == STARTED)) state = STARTED;
+	else if(running)                                       state = RUNNING;
+	else if(state != ARMED)                                state = IDLE;
 
 	// State machine 
 	switch(state) {
@@ -85,6 +84,7 @@ long cta_state_machine(aSubRecord* prec) {
 			*out_index_global = 0;             // reset index
 			*out_stop         = 0;             // reset stop button
 			cycles_cnt        = 0;             // reset CTA sequenc cycle counter
+			saved_pid         = 0;             // reset saved pid
 			break;
 
 		case RUNNING:
@@ -93,6 +93,8 @@ long cta_state_machine(aSubRecord* prec) {
         		*out_index_global = index;
 			// CTA sequence end
     			if(index >= length) {
+				// Manages cycles
+				// 0 = forever | >0 = nb cycles
 				++cycles_cnt;
 				if(cycles && cycles_cnt >= cycles) {
 					state           = STOPPED;
@@ -107,26 +109,39 @@ long cta_state_machine(aSubRecord* prec) {
     			}
     			else 
         			*out_enable_evt = 1;
-			if(last_state == STARTED)
+			if(last_state == STARTED) 
 				*out_started_at = pid;     // update starting pid
     			break;
 
 		case STARTED:
-			*out_enable_evt = 1;               // enable events
-			*out_running    = 1;               // update status 'running'
-			*out_start      = 0;               // reset start button
+			// Manages configuration mode 
+			//
+			// Uses next PID because aSub processes after events sequence
+			next_pid = pid + 1;
+			//
+			// MODE: 0 = start immediately | 1 = start with divisor and offset
+			// Divisor
+			if(cfgMod && !saved_pid && (next_pid % cfgModDiv)==0 ) 
+				saved_pid = next_pid;
+			// Offset 
+			if(!cfgMod || (next_pid - saved_pid == cfgModOff)) {
+				*out_enable_evt = 1;       // enable events
+				*out_running    = 1;       // update status 'running'
+				*out_start      = 0;       // reset start button
+			}
 			break;
 
 		case ARMED:
 
 		case IDLE:
-			// Sequence update
+			// Load sequence
 			if(loadSeqPending){
 				*out_load_seq         = 1; // load sequence flag
 				*out_load_seq_pending = 0; // reset load seq pending flag
 			}
 			// Pulse ID synchronisation 
-			if(pid == last_pid+1)
+			// makes sure the pulse ID as changed because jitter can prevent that 
+			if(pid == last_pid + 1)
 				state = ARMED;
 			else 
 				state = IDLE;
