14155bd410175265095353fe043d8286f1c8b057
Romain Vallotton <romain.vallotton@psi.ch>
2026-06-11 13:49:18 +0200

    [UPDATE] start_cta_gui.sh with deployment macros for production

--- ui/start_cta_gui.sh
+++ ui/start_cta_gui.sh
@@ -5,41 +5,50 @@ BLUE="\033[1;34m"
 GREEN="\033[1;32m"
 RESET="\033[0m"
 
-# Find real absolute path of the python file 
-BASEDIR="$(dirname "$(readlink -f "$0")")"
+# CTA GUI directory
+#----------------------------------------------------
+CTA_GUI_DIR="/sf/controls/bin"
+
+# Conda environment with cta_lib and its dependencies
+#----------------------------------------------------
+CONDA_ENV="/sf/controls/applications/cta_lib"
+PYTHON="$CONDA_ENV/bin/python"
 
 DEV_MODE=0
 
 if [ "$1" = "--dev" ]; then
-    DEV_MODE=1
-    shift
+	DEV_MODE=1
+	PYTHON=python
+	shift
 fi
 
 # Check python installation 
 #----------------------------------------------------
-if ! command -v python >/dev/null 2>&1; then
-    echo "ERROR: Python not found in PATH."
-    exit 1
+if ! command -v "$PYTHON" >/dev/null 2>&1; then
+	echo "ERROR: Python not found in $PYTHON"
+	exit 1
 fi
 
 # Check CTA installation 
 #----------------------------------------------------
-if ! python -c "import cta_lib; import PyQt5" >/dev/null 2>&1; then
-    echo "ERROR: CTA Python environment or PyQt5 not detected."
-    exit 1
+if ! "$PYTHON" -c "import cta_lib; import PyQt5" >/dev/null 2>&1; then
+	echo "ERROR: CTA Python environment or PyQt5 not detected."
+	exit 1
 fi
 
 # Dev mode, PYTHONPATH gives priority to local folder
 #----------------------------------------------------
 if [ "$DEV_MODE" -eq 1 ]; then
-    export PYTHONPATH="$(dirname "$BASEDIR")/lib:$PYTHONPATH"
+	# Find real absolute path of the python file 
+	CTA_GUI_DIR="$(dirname "$(readlink -f "$0")")"
+	export PYTHONPATH="$(dirname "$CTA_GUI_DIR")/lib:$PYTHONPATH"
 fi
 
 if [ $# -eq 0 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 	echo -e "${BLUE}Wrapper options:${RESET}"
-	echo -e "  ${GREEN}--dev${RESET}	Use Local development cta_lib instead of installed package"
+	echo -e "  ${GREEN}--dev${RESET}	Use local cta_lib source tree and current Python environment"
 	echo 
-	exec python "$BASEDIR/cta_gui.py" --help
+	exec "$PYTHON" "$CTA_GUI_DIR/cta_gui.py" --help
 fi
 
-exec python "$BASEDIR/cta_gui.py" "$@"
+exec "$PYTHON" "$CTA_GUI_DIR/cta_gui.py" "$@"
